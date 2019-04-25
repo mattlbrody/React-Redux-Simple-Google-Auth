406bfa15f610a02bbe32ab2a2c522ae75b9262e6
Matthew Brody <matt@Matthews-MacBook-Air-3.local>
2019-04-25 10:45:27 +1000

    first commit

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+
+const App = () => {
+  return (
+    <div className="ui container">
+        <div>
+          <Header />
+        </div>
+    </div>
+  )
+}
+
+export default App;
